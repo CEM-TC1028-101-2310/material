142480ad1b93ccf8c2634862cf7641460a65a69e
aram-gonzalez-tec <aram.gonzalez@tec.mx>
2023-01-09 10:49:23 -0600

    Bibliotecas

---
+++ variables/bibliotecas.py
@@ -0,0 +1,9 @@
+# se importa toda la biblioteca y se usa como biblioteca.función()
+import math
+# sólo se importa lo que se indica de forma explícita
+# from biblioteca import función
+# from math import sqrt, sin
+
+x = math.sqrt(4)
+
+print(x)
---
+++ variables/dados.py
@@ -0,0 +1,12 @@
+# Aram Baruch González Pérez 8481611
+# Pedro Páramo Solís 621626
+# Actividad 01: Secuenciales
+# Ejercicio 1
+
+import random
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+
+print(f"El primer dado salió: {dado1}")
+print(f"El segundo dado salió: {dado2}")
